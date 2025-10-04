61cf5c5b5366db68989b544492f7824305204b63
Ary123-start <arianneclaudia@gmail.com>
2025-10-04 18:25:40 -0300

    ex14.js

---
+++ ex14.js
@@ -0,0 +1,13 @@
+// 14) Leia um número N. Use for para somar todos os números de 1 até N.
+
+
+const readline = require("readline-sync");
+
+let N = Number(readline.question("insira um numero:"));
+let soma = 0;
+
+for ( let i = 1; i  <= N; i++){//
+    soma = soma +i
+}
+console.log("soma de 1 ate " + N + " e " + soma );// aqui o terminal ficou tudo grudado
+// porque precisa dar um espaço nas aspas pra deixar o terminal organizado
